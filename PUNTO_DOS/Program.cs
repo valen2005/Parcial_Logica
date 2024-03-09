5d328fec37f8b5836a50eb795d66ab436e8e947e
valen2005 <gavalentina365@gmail.com>
2024-03-08 21:24:12 -0500

    primero se define la variable

---
+++ PUNTO_DOS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Definir la velocidad de cada tren en km/h
+        int velocidadTren1 = 80;
+        int velocidadTren2 = 100;
+
+        // Definir el tiempo inicial de cada tren en horas
+        double tiempoTren1 = 0;
+        double tiempoTren2 = 1; 
+    }
+
